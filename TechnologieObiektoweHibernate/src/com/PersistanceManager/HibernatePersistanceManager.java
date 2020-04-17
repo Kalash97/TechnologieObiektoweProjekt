ca69178ba01d5dbde21e6a733468ba88b0cacbd3
Kalash97 <xvjdpl@wp.pl>
2020-04-17 16:20:10 +0200

    Etap 3

--- TechnologieObiektoweHibernate/src/com/PersistanceManager/HibernatePersistanceManager.java
+++ TechnologieObiektoweHibernate/src/com/PersistanceManager/HibernatePersistanceManager.java
@@ -23,16 +23,6 @@ public class HibernatePersistanceManager {
 		em.close();
 	}
 	
-	//TMP
-//	public void delete(long id, Class<?> type) {
-//		EntityManager em = HibernateConnection.getManager();
-//		em.getTransaction().begin();
-//		Persistable persistable = (Persistable) em.find(type, id);
-//		em.remove(persistable);
-//		em.getTransaction().commit();
-//		em.close();
-//	}
-	
 	public Persistable findById(long id, Class<?> type) {
 		EntityManager em = HibernateConnection.getManager();
 		em.getTransaction().begin();
